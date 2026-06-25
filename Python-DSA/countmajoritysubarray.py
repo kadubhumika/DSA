acf2ebc9043a23d4c7cabb2fb275850134b2ca4c
BHUMIKA KADU✨ <kadubhumika2468@gmail.com>
2026-06-25 15:01:08 +0530

    countandsay

---
+++ Python-DSA/countmajoritysubarray.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def countMajoritySubarrays(self, nums: List[int], target: int) -> int:
+        # okay i understood the question
+        n = len(nums)
+        ans = 0
+        for i in range(n):
+            cnt = 0
+            for j in range(i, n):
+                cnt += 1 if nums[j] == target else -1
+                if cnt > 0:
+                    ans += 1
+        return ans
+
+
