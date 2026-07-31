172c63873f56d8c0fa99adccd45d6d3450b6c0ad
BHUMIKA KADU✨ <kadubhumika2468@gmail.com>
2026-07-31 23:32:58 +0530

    binary-traversal-tree.py

---
+++ Python-DSA/binary-traversal-tree.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+from collections import deque
+from typing import Optional, List
+
+
+class TreeNode:
+     def __init__(self, val=0, left=None, right=None):
+         self.val = val
+         self.left = left
+         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+
+        output = []
+        queue = deque([root])
+
+        while queue:
+            level_size = len(queue)
+            current_level = []
+
+            for _ in range(level_size):
+                node = queue.popleft()
+                current_level.append(node.val)
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            output.append(current_level)
+        return output
+
+
